9e43b35fd6089c84bf4a927aaa12330f24a10767
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2023-12-03 22:28:33 +0300

    => [ init ]

---
+++ JS-8kyu-01-Even or Odd.js
@@ -0,0 +1,16 @@
+/*
+  Kata-name => Even or Odd
+
+Description => Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+*/ //=========================================================
+
+function even_or_odd(number) {
+  if (number % 2 == 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+console.log(even_or_odd(10));
