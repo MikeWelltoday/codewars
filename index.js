499ae903f8d7fadbc019e2ecda776206b5ea4c6d
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2023-12-09 15:19:00 +0300

    => saved

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-function printerError(s: string): string {
+function printerError(s) {
 
     let count = 0;
 
     for (let i = 0; i <s.length; i++) {
-        // if (/[n-z]/.test(s[i])) {
-        //     count++
-        // }
-        console.log(i[s])
+
+        if(/[n-z]/.test(s[i])) {
+            count++
+        }
+
     }
 
 
---
+++ test.js
@@ -0,0 +1,16 @@
+function printerError(s: ): string {
+
+    let count = 0;
+
+    for (let i = 0; i <s.length; i++) {
+        // if (/[n-z]/.test(s[i])) {
+        //     count++
+        // }
+        console.log(i[s])
+    }
+
+
+    return `${count}/${s.length}`
+}
+
+console.log(printerError("aaaxbbbbyyhwawiwjjjwwm"))
